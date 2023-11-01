f76be3a9b267a94dd6260bd0dee81cae6179250e
Sulkybug <122849768+Sulkybug@users.noreply.github.com>
2023-11-01 01:33:32 -0500

    Update timeCalculator.js
    Deleting some comments

--- timeCalculator.js
+++ timeCalculator.js
@@ -102,16 +102,16 @@ function add_time() {
 }
 
 // Returns: 6:10 PM
-add_time("3:00 PM", "3:10"); // works
+console.log(add_time("3:00 PM", "3:10")); 
 
 // Returns: 2:02 PM, Monday
-add_time("11:30 AM", "2:32", "Monday"); // works
+add_time("11:30 AM", "2:32", "Monday"); 
 
 // Returns: 12:03 PM
-add_time("11:43 AM", "00:20"); // works
+add_time("11:43 AM", "00:20"); 
 
 // Returns: 1:40 AM (next day)
-add_time("10:10 PM", "3:30"); // works
+add_time("10:10 PM", "3:30"); 
 
 //Returns: 12:03 AM, Thursday (2 days later)
 add_time("11:43 PM", "24:20", "tueSday");
